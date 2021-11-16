2b215b08ac9efbccc49942f72558c3e574b7c935
montoyajdev <montoyajdev@gmail.com>
2021-11-16 15:26:15 -0800

    Implemented Gorilla Scheme package and used it to parse SignupForm

--- controllers/users.go
+++ controllers/users.go
@@ -4,6 +4,8 @@ import (
 	"bizzy_track/views"
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/schema"
 )
 
 /* NewUsers is used to create a new Users controller
@@ -26,15 +28,23 @@ func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 	u.NewView.Render(w, nil)
 }
 
+type SignupForm struct {
+	Email    string `schema:"email"`
+	Password string `schema:"password"`
+}
+
 // Create processes the signup form when a user submits it. Ultimately creates a new user account
 // POST /signup
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
 		panic(err)
 	}
-	fmt.Fprintln(w, r.PostForm["email"])
-	fmt.Fprintln(w, r.PostFormValue("email"))
-	fmt.Fprintln(w, r.PostForm["password"])
-	fmt.Fprintln(w, r.PostFormValue("password"))
+
+	dec := schema.NewDecoder()
+	var form SignupForm
+	if err := dec.Decode(&form, r.PostForm); err != nil {
+		panic(err)
+	}
+	fmt.Fprintln(w, form)
 
 }
