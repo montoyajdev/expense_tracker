9033d1f2d40fe990f184434245a59b9be2e04946
montoyajdev <montoyajdev@gmail.com>
2021-11-16 21:57:23 -0800

    DRY parsing form

--- controllers/users.go
+++ controllers/users.go
@@ -4,8 +4,6 @@ import (
 	"bizzy_track/views"
 	"fmt"
 	"net/http"
-
-	"github.com/gorilla/schema"
 )
 
 /* NewUsers is used to create a new Users controller
@@ -36,15 +34,9 @@ type SignupForm struct {
 // Create processes the signup form when a user submits it. Ultimately creates a new user account
 // POST /signup
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
-	if err := r.ParseForm(); err != nil {
-		panic(err)
-	}
-
-	dec := schema.NewDecoder()
 	var form SignupForm
-	if err := dec.Decode(&form, r.PostForm); err != nil {
+	if err := parseForm(r, &form); err != nil {
 		panic(err)
 	}
 	fmt.Fprintln(w, form)
-
 }
