4856e2f205a8d52cf58ccb3d269d66c99aef2b55
montoyajdev <montoyajdev@gmail.com>
2021-11-16 22:49:38 -0800

    Created controller for static pages

--- controllers/helpers.go
+++ controllers/helpers.go
@@ -7,12 +7,13 @@ import (
 )
 
 func parseForm(r *http.Request, dst interface{}) error {
+	// r.Parseform = map[string][]string
+	// checks if r.Parseform returns an error
 	if err := r.ParseForm(); err != nil {
 		return err
 	}
 
 	dec := schema.NewDecoder()
-
 	if err := dec.Decode(dst, r.PostForm); err != nil {
 		return err
 	}
